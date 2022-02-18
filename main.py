73eeec3760d9dacf80806d0e09477e1157c74682
Angelo Leite <angelolgoes13@gmail.com>
2022-02-18 19:12:59 -0300

    Adde try except to handle null frames

--- main.py
+++ main.py
@@ -49,140 +49,146 @@ while True:
 
     _, frame = cap.read() 
 
-    if screen_area == 0:
-        (screen_w, screen_h, _) = frame.shape
-        screen_area = screen_w * screen_h
+    try:
 
-    start = time.time()
-    classes, scores, boxes = model.detect(frame, 0.1, 0.2)
-    end = time.time()
+        if screen_area == 0:
+            (screen_w, screen_h, _) = frame.shape
+            screen_area = screen_w * screen_h
 
-    objects = zip(classes, scores, boxes)
-    objects = [x for x in objects if x[1] > minscore]
+        start = time.time()
+        classes, scores, boxes = model.detect(frame, 0.1, 0.2)
+        end = time.time()
 
-    xpos = 0
-    largest_area = 0
-    largest_label = ""
-    distance = 0
-    angle = 0
-    largest_box = []
+        objects = zip(classes, scores, boxes)
+        objects = [x for x in objects if x[1] > minscore]
+
+        xpos = 0
+        largest_area = 0
+        largest_label = ""
+        distance = 0
+        angle = 0
+        largest_box = []
 
 
-    for (classid, score, box) in objects:
-        area = box[2] * box[3]
+        for (classid, score, box) in objects:
+            area = box[2] * box[3]
 
-        # its the largest object
-        if area > largest_area:
-            largest_area = area
-            largest_box = box
-            largest_label = class_names[classid[0]]
+            # its the largest object
+            if area > largest_area:
+                largest_area = area
+                largest_box = box
+                largest_label = class_names[classid[0]]
 
-        color = colors[int(classid) % len(colors)]
-        label = f"{class_names[classid[0]]} : {score}"
-        cv2.rectangle(frame, box, color, 2)
-        cv2.putText(frame, label, (box[0], box[1] - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, color, 1)
+            color = colors[int(classid) % len(colors)]
+            label = f"{class_names[classid[0]]} : {score}"
+            cv2.rectangle(frame, box, color, 2)
+            cv2.putText(frame, label, (box[0], box[1] - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, color, 1)
+        
+
+        if len(largest_box) > 0:
+            xpos = largest_box[0] + int(largest_box[2]/2)
+            ypos = largest_box[1] + int(largest_box[3])
+            midy = largest_box[1] + int(largest_box[3]/2)
+            #ypos = int(3*screen_w/4)
+
+            # proportion by area
+            proportion_area = 2*largest_area/screen_area
+            #proportion by ypos
+            proportion_ypos = ypos/screen_h
+
+            proportion = min(proportion_area + proportion_ypos, 1)
+
+            close_meter = min(int(255 * proportion), 255)
+            tickness = min(max(int(5 * proportion), 2), 5)
+            distance = max(round((5 - 5 * proportion), 2), 0)
+
+            # Add one to avoid division by zero
+            if xpos < screen_h/2 : angle = round(degrees(atan((screen_h/2 - xpos)/(midy + 1))) + 90, 2)
+            else: angle = round(degrees(atan(midy/(xpos - screen_h/2 + 1))), 2)
+
+            cv2.arrowedLine(frame, (int(screen_h/2), screen_w), (xpos, midy), black, tickness+5)
+            cv2.arrowedLine(frame, (int(screen_h/2), screen_w), (xpos, midy), (0, 255 - close_meter, close_meter), tickness)
+
+
+        should_avoid = False
+        if  distance < collision_distance and \
+            angle < (90 + collision_fov/2) and \
+            angle > (90 - collision_fov/2):
+            should_avoid = True
+
+
+        if obj_append_counter >= obj_append_delay:
+            if should_avoid: 
+                to_append = (largest_label, distance, angle)
+            else: to_append = null_object
+            object_stack.append(to_append)
+            object_stack.pop(0)
+            obj_append_counter = 0
+        else: obj_append_counter += 1
+
+
+        frame = cv2.copyMakeBorder(frame, 0, 300, 0, 0, cv2.BORDER_CONSTANT, value=gray)
+
+        focus_obj = f"Focusing now on: {largest_label}"
+        focus_distance = f"Aproximate distance: {distance} meters"
+        focus_angle = f"Aproximate angle: {angle} degrees"
+        avoid_flag = f"Should avoid? {should_avoid}"
+        fps_label = f"FPS: {round((1.0/(end - start)), 2)}"
+        
+
+        if largest_label == "":
+            focus_obj = f"Focusing now on: Nothing"
+            focus_distance = f"Aproximate distance: None"
+            focus_angle = f"Aproximate angle: None"
+            avoid_flag = f"Should avoid? {False}"
+        
+        cv2.putText(frame, fps_label, (0, 20), cv2.FONT_HERSHEY_SIMPLEX, 0.8, black, 4)
+        cv2.putText(frame, fps_label, (0, 20), cv2.FONT_HERSHEY_SIMPLEX, 0.8, green, 2)
+        cv2.putText(frame, focus_obj, (10, screen_w + 20), cv2.FONT_HERSHEY_SIMPLEX, 0.5, white, 1)
+        cv2.putText(frame, focus_distance, (10, screen_w + 40), cv2.FONT_HERSHEY_SIMPLEX, 0.5, white, 1)
+        cv2.putText(frame, focus_angle, (10, screen_w + 60), cv2.FONT_HERSHEY_SIMPLEX, 0.5, white, 1)
+        cv2.putText(frame, avoid_flag, (10, screen_w + 80), cv2.FONT_HERSHEY_SIMPLEX, 0.5, white, 1)
+        cv2.putText(frame, memory_label, (10, screen_w + 120), cv2.FONT_HERSHEY_SIMPLEX, 0.6, white, 2)
+        
+
+        # Initializing direction arow colors
+        lcolor = green
+        rcolor = green
+        ucolor = green
+
+
+        # Display memory queue
+        # Updating arrow colors depending on possible collisions
+        for i in range(len(object_stack)):
+            obj_label = f"{i+1} : {object_stack[i][0]} -> {object_stack[i][1]} m | {object_stack[i][2]} dgrs"
+            cv2.putText(frame, obj_label, (10, screen_w + 150 + i*15), cv2.FONT_HERSHEY_SIMPLEX, 0.4, white, 1)
+            if object_stack[i][0] != "null":
+                if   object_stack[i][2] < (90 - frontal_collision_fov/2):   rcolor = red
+                elif object_stack[i][2] > (90 + frontal_collision_fov/2):   lcolor = red
+                else:                                                       ucolor = red
+
+
+        # Display left arrow
+        cv2.arrowedLine(frame, (int(5*screen_h/8) - x_padding, screen_w + y_padding), (int(screen_h/2) - x_padding, screen_w + y_padding), white, 25, tipLength=0.5)
+        cv2.arrowedLine(frame, (int(5*screen_h/8) - x_padding, screen_w + y_padding), (int(screen_h/2) - x_padding, screen_w + y_padding), lcolor, 15, tipLength=0.5)
+        cv2.putText(frame, turn_left_label, (int(screen_h/2) - x_padding, screen_w + y_padding + 70), cv2.FONT_HERSHEY_SIMPLEX, 0.5, black, 2)
+
+        # Display right arrow
+        cv2.arrowedLine(frame, (int(3*screen_h/4) - x_padding, screen_w + y_padding), (int(7*screen_h/8) - x_padding, screen_w + y_padding), white, 25, tipLength=0.5)
+        cv2.arrowedLine(frame, (int(3*screen_h/4) - x_padding, screen_w + y_padding), (int(7*screen_h/8) - x_padding, screen_w + y_padding), rcolor, 15, tipLength=0.5)
+        cv2.putText(frame, turn_right_label, (int(3*screen_h/4) - x_padding, screen_w + y_padding + 70), cv2.FONT_HERSHEY_SIMPLEX, 0.5, black, 2)
+
+        # Display up arrow
+        cv2.arrowedLine(frame, (int(.6875*screen_h) - x_padding, screen_w + y_padding - 30), (int(.6875*screen_h) - x_padding, int(.75*screen_w + y_padding - 30)), white, 25, tipLength=0.5)
+        cv2.arrowedLine(frame, (int(.6875*screen_h) - x_padding, screen_w + y_padding - 30), (int(.6875*screen_h) - x_padding, int(.75*screen_w + y_padding - 30)), ucolor, 15, tipLength=0.5)
+        cv2.putText(frame, straight_label, (int(.6875*screen_h) - x_padding - 50, int(.75*screen_w + y_padding - 60)), cv2.FONT_HERSHEY_SIMPLEX, 0.5, black, 2)
+        
+
+        cv2.imshow("SYLVIA", frame)
     
-
-    if len(largest_box) > 0:
-        xpos = largest_box[0] + int(largest_box[2]/2)
-        ypos = largest_box[1] + int(largest_box[3])
-        midy = largest_box[1] + int(largest_box[3]/2)
-        #ypos = int(3*screen_w/4)
-
-        # proportion by area
-        proportion_area = 2*largest_area/screen_area
-        #proportion by ypos
-        proportion_ypos = ypos/screen_h
-
-        proportion = min(proportion_area + proportion_ypos, 1)
-
-        close_meter = min(int(255 * proportion), 255)
-        tickness = min(max(int(5 * proportion), 2), 5)
-        distance = max(round((5 - 5 * proportion), 2), 0)
-
-        # Add one to avoid division by zero
-        if xpos < screen_h/2 : angle = round(degrees(atan((screen_h/2 - xpos)/(midy + 1))) + 90, 2)
-        else: angle = round(degrees(atan(midy/(xpos - screen_h/2 + 1))), 2)
-
-        cv2.arrowedLine(frame, (int(screen_h/2), screen_w), (xpos, midy), black, tickness+5)
-        cv2.arrowedLine(frame, (int(screen_h/2), screen_w), (xpos, midy), (0, 255 - close_meter, close_meter), tickness)
-
-
-    should_avoid = False
-    if  distance < collision_distance and \
-        angle < (90 + collision_fov/2) and \
-        angle > (90 - collision_fov/2):
-        should_avoid = True
-
-
-    if obj_append_counter >= obj_append_delay:
-        if should_avoid: 
-              to_append = (largest_label, distance, angle)
-        else: to_append = null_object
-        object_stack.append(to_append)
-        object_stack.pop(0)
-        obj_append_counter = 0
-    else: obj_append_counter += 1
-
-
-    frame = cv2.copyMakeBorder(frame, 0, 300, 0, 0, cv2.BORDER_CONSTANT, value=gray)
-
-    focus_obj = f"Focusing now on: {largest_label}"
-    focus_distance = f"Aproximate distance: {distance} meters"
-    focus_angle = f"Aproximate angle: {angle} degrees"
-    avoid_flag = f"Should avoid? {should_avoid}"
-    fps_label = f"FPS: {round((1.0/(end - start)), 2)}"
-    
-
-    if largest_label == "":
-        focus_obj = f"Focusing now on: Nothing"
-        focus_distance = f"Aproximate distance: None"
-        focus_angle = f"Aproximate angle: None"
-        avoid_flag = f"Should avoid? {False}"
-    
-    cv2.putText(frame, fps_label, (0, 20), cv2.FONT_HERSHEY_SIMPLEX, 0.8, black, 4)
-    cv2.putText(frame, fps_label, (0, 20), cv2.FONT_HERSHEY_SIMPLEX, 0.8, green, 2)
-    cv2.putText(frame, focus_obj, (10, screen_w + 20), cv2.FONT_HERSHEY_SIMPLEX, 0.5, white, 1)
-    cv2.putText(frame, focus_distance, (10, screen_w + 40), cv2.FONT_HERSHEY_SIMPLEX, 0.5, white, 1)
-    cv2.putText(frame, focus_angle, (10, screen_w + 60), cv2.FONT_HERSHEY_SIMPLEX, 0.5, white, 1)
-    cv2.putText(frame, avoid_flag, (10, screen_w + 80), cv2.FONT_HERSHEY_SIMPLEX, 0.5, white, 1)
-    cv2.putText(frame, memory_label, (10, screen_w + 120), cv2.FONT_HERSHEY_SIMPLEX, 0.6, white, 2)
-    
-
-    # Initializing direction arow colors
-    lcolor = green
-    rcolor = green
-    ucolor = green
-
-
-    # Display memory queue
-    # Updating arrow colors depending on possible collisions
-    for i in range(len(object_stack)):
-        obj_label = f"{i+1} : {object_stack[i][0]} -> {object_stack[i][1]} m | {object_stack[i][2]} dgrs"
-        cv2.putText(frame, obj_label, (10, screen_w + 150 + i*15), cv2.FONT_HERSHEY_SIMPLEX, 0.4, white, 1)
-        if object_stack[i][0] != "null":
-            if   object_stack[i][2] < (90 - frontal_collision_fov/2):   rcolor = red
-            elif object_stack[i][2] > (90 + frontal_collision_fov/2):   lcolor = red
-            else:                                                       ucolor = red
-
-
-    # Display left arrow
-    cv2.arrowedLine(frame, (int(5*screen_h/8) - x_padding, screen_w + y_padding), (int(screen_h/2) - x_padding, screen_w + y_padding), white, 25, tipLength=0.5)
-    cv2.arrowedLine(frame, (int(5*screen_h/8) - x_padding, screen_w + y_padding), (int(screen_h/2) - x_padding, screen_w + y_padding), lcolor, 15, tipLength=0.5)
-    cv2.putText(frame, turn_left_label, (int(screen_h/2) - x_padding, screen_w + y_padding + 70), cv2.FONT_HERSHEY_SIMPLEX, 0.5, black, 2)
-
-    # Display right arrow
-    cv2.arrowedLine(frame, (int(3*screen_h/4) - x_padding, screen_w + y_padding), (int(7*screen_h/8) - x_padding, screen_w + y_padding), white, 25, tipLength=0.5)
-    cv2.arrowedLine(frame, (int(3*screen_h/4) - x_padding, screen_w + y_padding), (int(7*screen_h/8) - x_padding, screen_w + y_padding), rcolor, 15, tipLength=0.5)
-    cv2.putText(frame, turn_right_label, (int(3*screen_h/4) - x_padding, screen_w + y_padding + 70), cv2.FONT_HERSHEY_SIMPLEX, 0.5, black, 2)
-
-    # Display up arrow
-    cv2.arrowedLine(frame, (int(.6875*screen_h) - x_padding, screen_w + y_padding - 30), (int(.6875*screen_h) - x_padding, int(.75*screen_w + y_padding - 30)), white, 25, tipLength=0.5)
-    cv2.arrowedLine(frame, (int(.6875*screen_h) - x_padding, screen_w + y_padding - 30), (int(.6875*screen_h) - x_padding, int(.75*screen_w + y_padding - 30)), ucolor, 15, tipLength=0.5)
-    cv2.putText(frame, straight_label, (int(.6875*screen_h) - x_padding - 50, int(.75*screen_w + y_padding - 60)), cv2.FONT_HERSHEY_SIMPLEX, 0.5, black, 2)
-    
-
-    cv2.imshow("SYLVIA", frame)
+    except:
+        print("Null frame")
+        break
 
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
